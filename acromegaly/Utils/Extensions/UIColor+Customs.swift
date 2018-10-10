a5b3d066e94777d46d8b4de2b39e862972647ff5
Hubert Andrzejewski <antrof@gmail.com>
2018-10-10 23:06:57 +0200

    BluetoothStatusView with animations of text change and background

---
+++ acromegaly/Utils/Extensions/UIColor+Customs.swift
@@ -0,0 +1,15 @@
+//
+//  UIColor+Customs.swift
+//  Acromegaly
+//
+//  Created by Hubert Andrzejewski on 10.10.2018.
+//  Copyright © 2018 Hubert Andrzejewski. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    static let systemTintBlue = UIColor(red: 0, green: 0.47, blue: 1, alpha: 1)
+    
+}
