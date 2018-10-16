7f40a4dcd3885703075b2f529e83243ca8391c49
Hubert Andrzejewski <antrof@gmail.com>
2018-10-16 09:56:09 +0200

    AppIcon

---
+++ acromegaly/Utils/Extensions/UIView+Nib.swift
@@ -0,0 +1,9 @@
+//
+//  UIView_Nib.swift
+//  Acromegaly
+//
+//  Created by Hubert Andrzejewski on 11.10.2018.
+//  Copyright © 2018 Hubert Andrzejewski. All rights reserved.
+//
+
+import Foundation
